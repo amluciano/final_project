698ef11bb72b5cb3d5b50e162f9f98ba70676cd4
Angela <amluciano@quinnipiac.edu>
2015-11-02 21:33:03 +0000

    Add Post to admin panel

--- core/views.py
+++ core/views.py
@@ -3,14 +3,14 @@ from django.core.urlresolvers import reverse_lazy
 from .models import *
 
 class Home(TemplateView):
-    template_name = "home.html"
+  template_name = "home.html"
 
 class PostCreateView(CreateView):
   model = Post
   template_name = "post/post_form.html"
   fields = ['title', 'description']
   success_url = reverse_lazy('home')
-  
+
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(PostCreateView, self).form_valid(form)
