402492660e46158983aea0d465d4361cbd7d7a56
Angela <amluciano@quinnipiac.edu>
2015-11-02 21:58:55 +0000

    Add post list view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+from django.views.generic import ListView
 
 class Home(TemplateView):
   template_name = "home.html"
@@ -9,8 +10,12 @@ class PostCreateView(CreateView):
   model = Post
   template_name = "post/post_form.html"
   fields = ['title', 'description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('post_list')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(PostCreateView, self).form_valid(form)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post/post_list.html"
