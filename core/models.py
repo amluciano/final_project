7fa530baf307f5cd4621cc7273ca1d6f37ec9415
Angela <amluciano@quinnipiac.edu>
2015-11-05 21:48:10 +0000

    Add comment voting

--- core/models.py
+++ core/models.py
@@ -7,25 +7,26 @@ class Post(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   user = models.ForeignKey(User)
-  
+
   def __unicode__(self):
     return self.title
 
   def get_absolute_url(self):
     return reverse("post_detail", args=[self.id])
-  
+
 class Comment(models.Model):
   post = models.ForeignKey(Post)
   user = models.ForeignKey(User)
   created_at = models.DateTimeField(auto_now_add=True)
   text = models.TextField()
-    
+
   def __unicode__(self):
     return self.text
-  
+
 class Vote(models.Model):
   user = models.ForeignKey(User)
-  post = models.ForeignKey(Post)
-  
+  post = models.ForeignKey(Post, blank=True, null=True)
+  comment = models.ForeignKey(Comment, blank=True, null=True)
+
   def __unicode__(self):
     return "%s upvoted" % (self.user.username)
