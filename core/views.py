6d41b1f7e584ca13d941e27081fee8fddae07fa5
Angela <amluciano@quinnipiac.edu>
2015-11-05 21:00:38 +0000

    Add post voting

--- core/views.py
+++ core/views.py
@@ -1,10 +1,9 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView, FormView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
-from django.views.generic import ListView
-from django.views.generic import DetailView
-from django.views.generic import UpdateView
-from django.views.generic import DeleteView
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render,redirect
+from .forms import *
 
 class Home(TemplateView):
   template_name = "home.html"
@@ -39,10 +38,22 @@ class PostUpdateView(UpdateView):
   template_name = 'post/post_form.html'
   fields = ['title', 'description']
 
+  def get_object(self, *args, **kwargs):
+    object = super(PostUpdateView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+    return object
+
 class PostDeleteView(DeleteView):
   model = Post
   template_name = 'post/post_confirm_delete.html'
   success_url = reverse_lazy('post_list')
+
+  def get_object(self, *args, **kwargs):
+    object = super(PostDeleteView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+      return object
 
 class CommentCreateView(CreateView):
   model = Comment
@@ -63,6 +74,12 @@ class CommentUpdateView(UpdateView):
   template_name = 'comment/comment_form.html'
   fields = ['text']
 
+  def get_object(self, *args, **kwargs):
+    object = super(CommentUpdateView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+      return object
+
   def get_success_url(self):
     return self.object.post.get_absolute_url()
 
@@ -70,6 +87,26 @@ class CommentDeleteView(DeleteView):
   model = Comment
   pk_url_kwarg = 'comment_pk'
   template_name = 'comment/comment_confirm_delete.html'
-  
+
   def get_success_url(self):
     return self.object.post.get_absolute_url()
+
+  def get_object(self, *args, **kwargs):
+    object = super(CommentDeleteView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+      return object
+
+class VoteFormView(FormView):
+  form_class = VoteForm
+
+  def form_valid(self, form):
+    user = self.request.user
+    post = Post.objects.get(pk=form.data["post"])
+    prev_votes = Vote.objects.filter(user=user, post=post)
+    has_voted = (prev_votes.count()>0)
+    if not has_voted:
+      Vote.objects.create(user=user, post=post)
+    else:
+      prev_votes[0].delete()
+    return redirect('post_list')
